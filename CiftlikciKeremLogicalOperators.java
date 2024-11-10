f153e21472abd53f202896de40bead179aa7009b
Kerem Erkut Çiftlikçi <76708644+KeremErkut@users.noreply.github.com>
2024-11-10 22:54:48 +0300

    Update CiftlikciKeremLogicalOperators.java

--- CiftlikciKeremLogicalOperators.java
+++ CiftlikciKeremLogicalOperators.java
@@ -3,7 +3,7 @@
 @Purpose:	To calculate the pizza order amount. For Lab4
 @Author:    Ciftlikci Kerem 
 @Date:   	11/10/24 | 10th of November 2024
-@Version:	1.0
+@Version:	1.1
 ********************************************************************/
 import java.util.Scanner;
 
@@ -20,27 +20,39 @@ public class CiftlikciKeremLogicalOperators {
             if (nToppings >= 0) {
                 break;
             } else {
-                System.out.println("The number of toppings must be more than 0");
+                System.out.println("The number of toppings must be 0 or more.");
             }
         }
         return nToppings;
     }
 
-    public static void comparenToppings(int nToppings) {
-        if (0 < nToppings && nToppings < 2) {
-            int firstResult = 6 + 2 * nToppings;
-            System.out.printf("Order amount is: $%d%n", firstResult);
-        } else if (nToppings == 2) {
-            int secondResult = 9;
-            System.out.printf("Order amount is: $%d%n", secondResult);
+    public static void calculateAmount(int nToppings) {
+        float pizzaPrice = 6.0f;
+        float toppingPrice;
+        
+        // Calculate topping price based on number of toppings
+        if (nToppings == 1) {
+            toppingPrice = nToppings * 2.0f;
+        } else if (nToppings >= 2) {
+            toppingPrice = nToppings * 1.5f;
         } else {
-            int thirdResult = (int) ((6 + 1.5 * nToppings) * 95 / 100);
-            System.out.printf("Order amount is: $%d%n", thirdResult);
+            toppingPrice = 0.0f;
         }
+
+        
+        float totalAmount = pizzaPrice + toppingPrice;
+        
+        
+        if (totalAmount > 10) {
+            totalAmount *= 0.95f;
+        }
+
+        
+        System.out.printf("Order amount is: $%.2f%n", totalAmount);
     }
-    
+
     public static void main(String[] args) {
         int nToppings = checknToppings();
-        comparenToppings(nToppings);
+        calculateAmount(nToppings);
     }  
 }
