5233a8b0bbcadf376d0ed4bea3629756995e52d3
Kerem Erkut Çiftlikçi <76708644+KeremErkut@users.noreply.github.com>
2024-11-12 22:23:28 +0300

    Add files via upload

---
+++ CiftlikciKeremAnimalSpeakingGreeting.java
@@ -0,0 +1,21 @@
+/*******************************************************************
+@Title:	    CiftlikciKeremAnimalSpeakingGreeting
+@Purpose:	To understand basics in java
+@Author:    Ciftlikci Kerem 
+@Date:   	11/11/24 | 11h of November 2024
+@Version:	1.0
+********************************************************************/
+public class CiftlikciKeremAnimalSpeakingGreeting{
+    
+    public static void drawAnimal(){
+        System.out.println("   /\\_/\\        -------");
+        System.out.println(" (   '   `  )   / Hello \\");
+        System.out.println(" (    -     )  < Newbie |");
+        System.out.println("   |   |   |    \\ Coder!/");
+        System.out.println(" (___|___)      -------");
+                       
+    }
+    public static void main(String[]args){
+        drawAnimal();
+    }
+}
