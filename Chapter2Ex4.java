09d690477f7e5fb5a09a760de0217c379fc29d0c
Kerem Erkut Çiftlikçi <76708644+KeremErkut@users.noreply.github.com>
2024-11-14 23:21:12 +0300

    Add files via upload

---
+++ Chapter2Ex4.java
@@ -0,0 +1,22 @@
+public class Chapter2Ex4 {
+    public static void main(String[] args) {
+        int value1 = 10; 
+        
+        double value2 = 19.9;
+        
+        byte value3 = 127;
+        
+        boolean status = true;
+
+        char letter = 'A';        
+
+        char letter2 =70;
+        
+        System.out.println("The first value is " + value1 );
+        System.out.println("The second value is " + value2 );
+        System.out.println("The third value is " + value3);
+        System.out.println("The status is " + status );
+        System.out.println("The letter is " + letter);
+        System.out.println("The second letter is " + letter2);
+    }
+}
