a8d147084e516e0d439b9a6bd3379cdeb6f02c94
Kerem Erkut Çiftlikçi <76708644+KeremErkut@users.noreply.github.com>
2024-11-15 18:02:14 +0000

    update CiftlikciKeremOddEven

--- CiftlikciKeremOddEven.java
+++ CiftlikciKeremOddEven.java
@@ -1,1 +1,35 @@
+import java.util.Scanner;
 
+public class CiftlikciKeremOddEven {
+
+    
+    public static int checkNumber() {
+        Scanner keyboard = new Scanner(System.in);
+        int number;
+        while (true) {
+            System.out.println("Please enter a positive number:");
+            number = keyboard.nextInt();
+
+            if (number > 0) {
+                break;
+            } else {
+                System.out.println("The number must be positive. Try again.");
+            }
+        }
+        return number;
+    }
+
+    
+    public static void isOddOrEven(int number) {
+        if ((number % 2) == 0) {
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");
+        }
+    }
+
+    public static void main(String[] args) {
+        int number = checkNumber(); 
+        isOddOrEven(number);        
+    }
+}
